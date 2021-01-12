461c82a345db71f20afc45ffb971980e02f69bb1
tonyg <aj.gianno@gmail.com>
2021-01-11 18:35:44 -0600

    catch; query builder; destroy method; dotenv

--- src/practice.js
+++ src/practice.js
@@ -10,14 +10,39 @@ const db = knex({
   // connection: protocol username:password @ domain name :port / path
   connection: process.env.DB_URL,
 });
-
-db.select('*')
-  .from('amazong_products')
-  .then((data) => {
+// essentially the equivalent of below, object directly from SQL. weak to SQL injects.
+db.raw(`SELECT * FROM amazong_products WHERE name = 'Flobulator 2000'`).then(
+  (data) => {
     console.log(data);
-  })
-  // explicitly state to end process if necessary using .finally() method
+  }
+);
 
-  .finally(() => {
-    db.destroy();
-  });
+//building a query
+
+// select * from products...
+
+function exampleOfQuery(inputName) {
+  //builder used to view SQL query
+  const builder = db('amazong_products')
+    .select('*')
+    // parameter prevents overwrite of SQL
+    .where({ name: inputName });
+
+  builder
+    .then((data) => {
+      console.log(data);
+    })
+    // error necessary to console knex error
+    .catch((err) => {
+      console.log(err);
+    })
+    // explicitly state to end process if necessary using .finally() method
+
+    .finally(() => {
+      db.destroy();
+      // log query as defined by builder, to see SQL
+      console.log(builder.toQuery());
+    });
+}
+
+exampleOfQuery('Flobulator 2000');
